090246db3ac666aa45d9db294ea7b79868859d96
NamNT <thanhnamdk2710@gmail.com>
2017-10-11 20:05:42 +0700

    Hello

--- app/User.php
+++ app/User.php
@@ -10,7 +10,7 @@ class User extends Authenticatable
     use Notifiable;
 
     /**
-     * The attributeasdsad sdads that are mass assignable.
+     * The attrisad  asdas dsdads that are mass assignable.
      *
      * @var array
      */
